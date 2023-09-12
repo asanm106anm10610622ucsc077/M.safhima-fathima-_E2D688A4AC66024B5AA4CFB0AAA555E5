cceb19f12342e090a50f5d3a3c05d657dc12ae7c
asanm106anm10610622ucsc077 <144545464+asanm106anm10610622ucsc077@users.noreply.github.com>
2023-09-12 19:21:42 +0530

    Add files via upload

--- main (5).py
+++ main (5).py
@@ -1,29 +1,25 @@
-class BankAccount:
+class Batsman:
 
-  def __init__(self, account_holder, balance=0):
-    self.account_holder = account_holder
-    self.balance = balance
+  def __init__(self, name):
+    self.name = name
 
-  def deposit(self, amount):
-    if amount > 0:
-      self.balance += amount
-      return f"Deposited ${amount}. New balance is ${self.balance}"
-    else:
-      return "Invalid deposit amount"
-
-  def withdraw(self, amount):
-    if 0 < amount <= self.balance:
-      self.balance -= amount
-      return f"Withdrew ${amount}. New balance is ${self.balance}"
-    else:
-      return "Insufficient funds or invalid withdrawal amount"
+  def play(self):
+    return f"{self.name} is batting"
 
 
-# Creating an instance of BankAccount
-account = BankAccount("John Doe")
+class Bowler:
 
-# Testing deposit and withdrawal functionality
-print(account.deposit(1000))  # Deposited $1000. New balance is $1000
-print(account.withdraw(500))  # Withdrew $500. New balance is $500
-print(
-    account.withdraw(1000))  # Insufficient funds or invalid withdrawal amount
+  def __init__(self, name):
+    self.name = name
+
+  def play(self):
+    return f"{self.name} is bowling"
+
+
+# Creating objects of Batsman and Bowler classes
+batsman = Batsman("John")
+bowler = Bowler("Alice")
+
+# Calling the play() method for each object
+print(batsman.play())  # John is batting
+print(bowler.play())  # Alice is bowling
